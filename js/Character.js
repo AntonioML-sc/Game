9013a19d8eaba4955f37ea57814205ac6aa216e0
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-05-28 11:33:14 +0200

    add movement to character in x axis

--- js/Character.js
+++ js/Character.js
@@ -1,10 +1,13 @@
 
+const WINDOWWIDTH = 1500;
+const WINDOWHEIGHT = 630;
+
 class Character {
     ///////////////////////////////  CLASS ATTRIBUTES  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
     name = '';
     hp = 100;
-    maxSpeed = 0;
+    maxSpeedX = 1.5;
     height = 40;
     width = 25;
     sx = 50;
@@ -19,6 +22,7 @@ class Character {
     leftBorder;
     rightBorder;
     fatherDiv = '';
+    div;
 
     ///////////////////////////////  CONSTRUCTOR  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
@@ -41,10 +45,49 @@ class Character {
         let make = document.createElement('div');
         make.setAttribute("id", this.name);
         document.getElementById(this.fatherDiv).appendChild(make);
-        
+
         // set styles to the charachter        
-        let myself = document.getElementById(this.name);
-        myself.setAttribute("style", `background-color: orange; width: ${this.width}px; height: ${this.height}px; position: absolute; top: ${this.sy}px; left: ${this.sx}px; z-index: 5;`);
+        this.div = document.getElementById(this.name);
+        this.div.setAttribute("style", `background-color: orange; width: ${this.width}px; height: ${this.height}px; position: absolute; top: ${this.sy}px; left: ${this.sx}px; z-index: 5;`);
+    }
+
+    setVx(direction) {
+        // set speed in x axis, based in commands and phisics
+        if (this.vy == 0) {
+            switch (direction) {
+                case 'right':
+                    this.vx = this.maxSpeedX;
+                    break;
+                case 'left':
+                    this.vx = -1 * this.maxSpeedX;
+                    break;
+                case 'stop':
+                    this.vx = 0;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    upgradePos() {
+        // calculate the new position based on speed and assign a new valid position  
+        this.sx += this.vx;
+        if (this.sx < 0) {
+            this.sx = 0;
+        }
+        if (this.sx > WINDOWWIDTH - this.width) {
+            this.sx = WINDOWWIDTH - this.width;
+        }
+
+        // upgrade borders positions
+        this.topBorder = this.sy;
+        this.bottomBorder = this.sy + this.height;
+        this.leftBorder = this.sx;
+        this.bottomBorder = this.sx + this.width;
+
+        // set the div in the assigned position
+        this.div.style.setProperty("left", `${pepe.sx}px`);
     }
 
 }
--- js/main.js
+++ js/main.js
@@ -2,15 +2,43 @@
 
 let time = document.getElementById("timer");
 
+let ir = true;
 let t = 0;
 
 let pepe = new Character("Pepe", 50, 590, "space");
+let pepeDiv = document.getElementById(pepe.name);
 
 function myfunction() {
-    t++;
-    console.log(t);
-    time.innerHTML = `Tiempo total: ${t}`;
+    t += 20;
+    
+    if (t % 1000 == 0) {
+        time.innerHTML = `Tiempo total: ${t / 1000}s`;
+
+        let pepeDivPosX = window.getComputedStyle(pepeDiv).getPropertyValue('left');
+        console.log("sx: " + pepe.sx + "posicion div: " + pepeDivPosX);
+    }
+
+    pepe.upgradePos();
 }
 
-let count = setInterval(myfunction, 1000);
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    if (keyName == 'd') {
+        pepe.setVx('right');
+    } else if (keyName == 'a') {
+        pepe.setVx('left');        
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    const keyName = event.key;
+    if (keyName == 'd') {
+        pepe.setVx('stop');
+    } 
+    if (keyName == 'a') {
+        pepe.setVx('stop');      
+    }
+});
+
+let count = setInterval(myfunction, 10);
 
