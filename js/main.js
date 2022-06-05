17d123e1ab0a8f39c1b8b10cc9edd107956022f4
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-06-05 15:10:38 +0200

    Add win condition in main.js

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ const pickCharacter = (character) => {
         if (!players.includes(characters[character])) {
             players.push(characters[character]);
         }
-        
+
         console.log(players);
 
         if (players.length == 2) {
@@ -74,32 +74,30 @@ const play = (players) => {
     // Instances
 
     const time = document.getElementById("timer");
-
     let t = 0;
+    const goalPosition = [1400, 100];
+
+    // Player 1
 
     const player1 = players[0];
     player1.render(50, 590, "space");
-
     const player1Portrait = document.getElementById("portraitP1");
     const player1LifeBar = document.getElementById("lifeP1");
-
     player1Portrait.setAttribute("style", `background-image: url(${players[0].portrait}); height: 15em; background-size: 47px 46px; background-repeat: no-repeat; background-position: top;`);
-    
+
+    // Player 2
     const player2 = players[1];
     player2.render(80, 590, "space");
-
     const player2Portrait = document.getElementById("portraitP2");
     const player2LifeBar = document.getElementById("lifeP2");
-    
     player2Portrait.setAttribute("style", `background-image: url(${players[1].portrait}); height: 15em; background-size: 47px 46px; background-repeat: no-repeat; background-position: top;`);
 
+    // Walls
     const wall0 = new Wall('wall0', "space", 500, 200, 240, 120, 'greenyellow', 3, 4, 4, 4, 4, 'black', 2, 2, 2, 2, '../img/stoneWall50x50.jpg');
-
     const wall1 = new Wall('wall1', "space", 500, 570, 60, 60, 'greenyellow', 3, 4, 4, 4, 4, 'black', 2, 2, 2, 2, '../img/stoneWall50x50.jpg');
     const wall2 = new Wall('wall2', "space", 150, 570, 10, 60, 'lightgrey', 3, 5, 5, 0, 0, 'black', 2, 2, 2, 2, '../img/greyStone50x50.jpg');
     const wall3 = new Wall('wall3', "space", 900, 530, 10, 100, 'lightgrey', 3, 5, 5, 0, 0, 'black', 2, 2, 2, 2, '../img/greyStone50x50.jpg');
     const wall4 = new Wall('wall4', "space", 1300, 430, 10, 100, 'lightgrey', 3, 5, 5, 0, 0, 'black', 2, 2, 2, 2, '../img/greyStone50x50.jpg');
-
     const wall5 = new Wall('wall5', "space", 600, 590, 60, 40, 'lightgrey', 3, 5, 5, 0, 0, 'black', 2, 2, 2, 2);
     const wall6 = new Wall('wall6', "space", 820, 590, 60, 40, 'lightgrey', 3, 5, 5, 0, 0, 'black', 2, 2, 2, 2);
     const falseWall1 = new Wall('falseWall1', "space", 640, 580, 200, 50, 'grey', 2, 5, 5, 0, 0, 'black', 2, 2, 2, 2);
@@ -110,7 +108,9 @@ const play = (players) => {
     const platform4 = new Wall('platform4', 'space', 1050, 200, 300, 15, 'brown', 3, 3, 3, 3, 3, 'black', 2, 2, 2, 2);
     const platform5 = new Wall('platform5', 'space', 1200, 100, 300, 15, 'brown', 3, 3, 3, 3, 3, 'black', 2, 2, 2, 2);
     const platform6 = new Wall('platform6', 'space', 700, 400, 300, 15, 'brown', 3, 3, 3, 3, 3, 'black', 2, 2, 2, 2);
+    const platform7 = new Wall('platform7', 'space', 950, 300, 120, 10, 'brown', 3, 3, 3, 3, 3, 'black', 2, 2, 2, 2);
 
+    // Traps
     const acid1 = new Fluid('acid1', "space", 660, 600, 160, 30, 'lime', 'lime', 0.35, 0.03, 0.7, 0.97);
     const toxicFog1 = new Fluid('toxicFog1', "space", 160, 580, 340, 50, 'fuchsia', 'fuchsia', 0.1, 0.02, 1, 1);
 
@@ -134,17 +134,15 @@ const play = (players) => {
 
 
     // arrays of objects to test players status
-    const contactObjectsP1 = [wall1, wall2, wall3, wall4, wall5, wall6, platform1, platform2, platform3, platform4, platform5, platform6, bird1, player2];
-    const contactObjectsP2 = [wall1, wall2, wall3, wall4, wall5, wall6, platform1, platform2, platform3, platform4, platform5, platform6, bird1, player1];
+    const contactObjectsP1 = [wall1, wall2, wall3, wall4, wall5, wall6, platform1, platform2, platform3, platform4, platform5, platform6, platform7, bird1, player2];
+    const contactObjectsP2 = [wall1, wall2, wall3, wall4, wall5, wall6, platform1, platform2, platform3, platform4, platform5, platform6, platform7, bird1, player1];
 
     const hazardZones = [acid1, toxicFog1, rollingRock1];
     const spikes = [spike1, spike2, spike3, spearFloor1, spearFloor2];
     const ceilingSpikes = [spikeCeiling1, spikeCeiling2, spikeCeiling3, spearCeiling1, spearCeiling2];
 
-    const upgradingItems = [spearCeiling1, spearCeiling2, spearFloor1, spearFloor2, rollingRock1, bird1];
+    const updatingItems = [spearCeiling1, spearCeiling2, spearFloor1, spearFloor2, rollingRock1, bird1];
 
-    console.log("player1 borders: top: " + player1.topBorder + " bottom: " + player1.bottomBorder + ", wall1 borders: top: " + wall1.topBorder + " bottom: " + wall1.bottomBorder);
-    console.log("wall1: sx, sy = " + wall1.sx + ", " + wall1.sy + ".  width, height: " + wall1.width + ", " + wall1.height);
 
     // Function that checks the status
 
@@ -153,18 +151,25 @@ const play = (players) => {
 
         if (t % 100 == 0) {
             time.innerHTML = `Tiempo total: ${t / 100}s`;
-
-            console.log("Player1 vida restante: ", Math.round(player1.hp));
         }
 
         // updates positions and displays the current hp of players
         player1.updatePos(contactObjectsP1, hazardZones, spikes, ceilingSpikes);
         player2.updatePos(contactObjectsP2, hazardZones, spikes, ceilingSpikes);
 
-        player1LifeBar.setAttribute("style", `background-color: lime; height: ${players[0].hp * 45 / players[0].maxHP }px; width: 100%; align-self: flex-end;`);
-        player2LifeBar.setAttribute("style", `background-color: lime; height: ${players[1].hp * 45 / players[1].maxHP }px; width: 100%; align-self: flex-end;`);
+        player1LifeBar.setAttribute("style", `background-color: lime; height: ${players[0].hp * 45 / players[0].maxHP}px; width: 100%; align-self: flex-end;`);
+        player2LifeBar.setAttribute("style", `background-color: lime; height: ${players[1].hp * 45 / players[1].maxHP}px; width: 100%; align-self: flex-end;`);
 
-        upgradingItems.forEach(i => i.updatePos(t));
+        updatingItems.forEach(i => i.updatePos(t));
+
+        for (let p of players) {
+            if ((p.sx > goalPosition[0]) && (p.sy <= goalPosition[1])) {
+                console.log("alguien ha llegado a la meta");
+                clearInterval(runGame);
+                winnerIs.innerHTML = `Ganador: ${p.name}`;
+                screenShift("screen5");
+            }
+        }
     }
 
     // Events that send orders for player1 movement
